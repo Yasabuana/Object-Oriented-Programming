f6af372ac5cca5a97e2814e9fc15616b9cba5fc9
Yasabuana <yasawijaya97@gmail.com>
2025-08-30 16:39:15 +0700

    Add files via upload

---
+++ Yasa_project1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.yasa_project1;
+
+/**
+ *
+ * @author yasaw
+ */
+public class Yasa_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("**********************************************");
+        System.out.println("* Nama   : Yasabuana Athallahaufa Natawijaya *");
+        System.out.println("* NPM    : 2420506030                        *");
+        System.out.println("* Alamat : Mertoyudan, Magelang              *");
+        System.out.println("* No_HP  : 088445243255                      *");
+        System.out.println("**********************************************");
+    }
+}
