08318ec883d8af231419afe1f5759542f7f5c5e1
Yasabuana <yasawijaya97@gmail.com>
2025-10-04 18:52:01 +0700

    Add files via upload

---
+++ ResponsiUTS_Yasa/src/main/java/com/mycompany/responsi_uts/Responsi_UTS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.responsi_uts;
+
+/**
+ *
+ * @author yasaw
+ */
+public class Responsi_UTS {
+
+    public static void main(String[] args) {
+        
+    }
+}
